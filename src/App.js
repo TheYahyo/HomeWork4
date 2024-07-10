b6cbf18414695d5bf7c3482187c77589b1c7b0f6
TheYahyo <oktamjanovyahyo11@gmail.com>
2024-07-10 06:21:41 -0600

    add names

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// import Count from "./Count";
+// import Button from "./Button";
+// import Cars from "./Cars";
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addName } from './redux/store';
 
 function App() {
+  const [name, setName] = useState('');
+  const names = useSelector(state => state.names.names);
+  const dispatch = useDispatch();
+
+  const handleAddName = () => {
+    if (name.trim()) {
+      dispatch(addName(name));
+      setName('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Count />
+      <Button />
+      <Cars /> */}
+      <h3>Names</h3>
+      <input
+        type="text"
+        placeholder="write the name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={handleAddName}>add</button>
+      <ul>
+        {names.map((name, index) => (
+          <h6 key={index}>{name}</h6>
+        ))}
+      </ul>
     </div>
+
   );
 }
-
 export default App;
---
+++ src/redux/index.js
@@ -0,0 +1,17 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import reducer from "./reducer";
+import cars from "./cars";
+
+
+const rootReducer = combineReducers({
+    reducer,
+    cars
+})
+
+export const store = configureStore({
+    reducer: rootReducer
+})
+
+
+
+
